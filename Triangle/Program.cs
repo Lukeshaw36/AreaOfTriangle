9797632b64ef3dbd67c22e1b8d234400f39a5b4e
Lukeshaw36 <103990852+Lukeshaw36@users.noreply.github.com>
2023-11-21 21:08:10 +0100

    Add files via upload
    Contains Code calculating Area of a triangle

---
+++ Triangle/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Triangle;
+
+public class AreaOfTriangle
+{
+    public static void Main(string[] args)
+    {
+        double area, 
+            
+         baselength,
+         height;
+
+
+        Console.Write("Enter the baselength of the triangle: ");
+        baselength = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the height of the triangle: ");
+        height = Convert.ToDouble(Console.ReadLine());
+
+        area = (height * baselength) / 2;
+        Console.WriteLine($"The area of your triangle is: {area} ");
+
+    }
+
+}
